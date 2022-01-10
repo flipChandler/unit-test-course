b3db7e85c9af012e70503c7e3f86acd6234b6277
flipChandler <felipe.santos11@hotmail.com>
2022-01-10 09:44:15 -0300

    exception handler for object not found created

--- src/main/java/br/com/dicasdeumdev/userapi/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/dicasdeumdev/userapi/services/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.userapi.services.impl;
 
 import br.com.dicasdeumdev.userapi.domains.User;
+import br.com.dicasdeumdev.userapi.services.exceptions.ObjectNotFoundException;
 import br.com.dicasdeumdev.userapi.repositories.UserRepository;
 import br.com.dicasdeumdev.userapi.services.UserService;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> optional = userRepository.findById(id);
-        return optional.orElse(null);
+        return optional.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
