814eb384e275c485c643f08ca4678d19a984c0c0
flipChandler <felipe.santos11@hotmail.com>
2022-01-10 17:51:23 -0300

    test UserResource findAll

--- src/test/java/br/com/dicasdeumdev/userapi/resources/UserResourceTest.java
+++ src/test/java/br/com/dicasdeumdev/userapi/resources/UserResourceTest.java
@@ -13,6 +13,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -38,6 +41,8 @@ class UserResourceTest {
     private UserServiceImpl userService;
 
     private User user;
+    private User user2;
+    private User user3;
     private UserDTO userDTO;
 
     @BeforeEach
@@ -67,7 +72,24 @@ class UserResourceTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAll_thenReturnAListOfUserDTO() {
+        when(userService.findAll()).thenReturn(List.of(user, user2, user3));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = userResource.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDTO.class, response.getBody().get(INDEX).getClass());
+        assertEquals(UserDTO.class, response.getBody().get(1).getClass());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        assertEquals(ID, response.getBody().get(INDEX).getId());
+        assertEquals(NAME, response.getBody().get(INDEX).getName());
+        assertEquals(EMAIL, response.getBody().get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(INDEX).getPassword());
     }
 
     @Test
@@ -84,6 +106,8 @@ class UserResourceTest {
 
     private void startUser() {
         user = new User(ID, NAME, EMAIL, PASSWORD);
+        user2 = new User(2, "Jack", "jack@gmail.com", "351");
+        user3 = new User(3, "Melanie", "mel@gmail.com", "753");
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
     }
 }
